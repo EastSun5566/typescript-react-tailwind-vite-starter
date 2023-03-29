186af4e485681712586c27e430387c1ac154bc63
汪東陽 <michael19920327@gmail.com>
2023-03-30 00:25:21 +0800

    chore: ignore eslint

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './App.css';
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
