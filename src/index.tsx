c259d1f7c5da54d560d6b88f14dd54adbc0e1cda
汪東陽 <michael19920327@gmail.com>
2023-03-30 00:37:23 +0800

    chore: add router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
 
 import App from './App';
-import './App.css';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
